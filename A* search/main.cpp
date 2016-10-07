76d0007ce18f2b73f69c34eb8ed0ef16e946df5a
郑昊 <zhenghao@d207-023-187-167.wireless.sfu.ca>
2016-10-07 15:24:38 -0700

    Initial Commit

---
+++ A* search/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  A* search
+//
+//  Created by 郑昊 on 2016-10-07.
+//  Copyright © 2016 haozheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
